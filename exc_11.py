3dabfb8c2c470ffcb86f6215fc9738f467ada1c6
Alexander Lazarev <arseysensector@gmail.com>
2016-06-18 17:10:11 +0300

    Update exc_11.py

--- exc_11.py
+++ exc_11.py
@@ -1,30 +1,17 @@
-print('Enter the 1st coordinate of castle\'s location')
-a = input()
-if a>8:
-    print('TYPE LOWER THAN 9, MOTHERFUCKER')
-    exit()
+
+a = input('Enter the 1st coordinate of castle\'s location')
+assert(a <= 8)
+b = input('Enter the 2nd coordinate of castle\'s location')
+assert(b <= 8)
+q = input('Enter the 1st coordinate of arrival point')
+assert(q <= 8)
+w = input('Enter the 2nd coordinate of arrival point')
+assert(2 <= 8)
+
+elif a==q or b==w:
+    print('Ye$$')
 else:
-    print('Enter the 2nd coordinate of castle\'s location')
-    b = input()
-    if b>8:
-        print('TYPE LOWER THAN 9, MOTHERFUCKER')
-        exit()
-    else:
-        print('Enter the 1st coordinate of arrival point')
-        q = input()
-        if q>8:
-            print('TYPE LOWER THAN 9, MOTHERFUCKER')
-            exit()
-        else:
-            print('Enter the 2nd coordinate of arrival point')
-            w = input()
-            if w>8:
-                print('TYPE LOWER THAN 9, MOTHERFUCKER')
-                exit()
-            elif a==q or b==w:
-                print('Ye$$')
-            else:
-                print('NO.')
+    print('NO.')
 
 
 
